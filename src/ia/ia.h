fce9ad091ac07ff875cc527986548ebc443a6561
siwon <simon.rousseau1@etu.univ-nantes.fr>
2012-01-26 13:22:00 +0100

    definition des strategie+1

--- src/ia/ia.h
+++ src/ia/ia.h
@@ -6,6 +6,8 @@ void initGameBoard(SZone* gameBoard);
 typedef enum {
 	strategieHome, //si tous les pions sont dans le home
 	strategieSure, //si on veut faire un mouvement sure mais pas intelligent
+	strategieAttaque, //si on veut que l'IA soit agressive
+	strategieEnAvance, //si on est en avance dans le jeu
 }strategie;
 
 /**
